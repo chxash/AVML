4aef4f78c2e78e7c9a94e64382365df10a733bc7
chxash <chxash@hotmail.com>
2013-10-19 15:50:54 +0200

    parse date and hour

--- src/test_parse.py
+++ src/test_parse.py
@@ -1,3 +1,5 @@
+#!/bin/python
+
 from bs4 import BeautifulSoup
 import urllib
 
@@ -23,10 +25,43 @@ txtToParse = h3.get_text()
 #collect raw datas
 print(txtToParse[30:])
 day = txtToParse[30:32]
-month = txtToParse[33:36]
-year = '0'
+month = txtToParse[33:37].upper()
+yearIndex = txtToParse[37:].index('2')
+year = txtToParse[37+yearIndex:41+yearIndex]
 print(day+' '+month+' '+year)
+print ()
+if (month=='JANV'):
+    obsDate = day+'/01/'+year
+elif (month == 'FEVR'):
+    obsDate = day+'/01/'+year
+elif (month == 'MARS'):
+    obsDate = day+'/03/'+year
+elif (month == 'AVRI'):
+    obsDate = day+'/04/'+year
+elif (month == 'MAI '):
+    obsDate = day+'/05/'+year
+elif (month == 'JUIN'):
+    obsDate = day+'/06/'+year
+elif (month == 'JUIL'):
+    obsDate = day+'/07/'+year
+elif (month == 'AOUT'):
+    obsDate = day+'/08/'+year
+elif (month == 'SEPT'):
+    obsDate = day+'/09/'+year
+elif (month == 'OCTO'):
+    obsDate = day+'/10/'+year
+elif (month == 'NOVE'):
+    obsDate = day+'/11/'+year
+elif (month == 'DECE'):
+    obsDate = day+'/02/'+year
+else: 
+    print 'couldnt read month'
 
+hourIndex = txtToParse[37:].index(':')
+obsHour = txtToParse[37+hourIndex-2:37+hourIndex+3]
+
+print(obsDate)
+print(obsHour)
 
 
 
